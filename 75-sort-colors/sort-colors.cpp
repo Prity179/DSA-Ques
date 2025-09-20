3c6a0fbffb9150fa8f715e1a337898f8558bfc0f
Prity179 <pritykumari12345000@gmail.com>
2025-09-20 22:11:32 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (49.09%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,19 +1,25 @@
-class Solution {
-public:
-    void sortColors(vector<int>& nums) {
-        unordered_map<int, int> count = {{0, 0}, {1, 0}, {2, 0}};
-
-        for (int num : nums) {
-            count[num]++;
-        }
-
+class Solution{
+    public:
+    void sortColors(vector<int>& nums){
+        int n = nums.size();
+        int left = 0;
         int idx = 0;
-        for (int color = 0; color < 3; color++) {
-            int freq = count[color];
-            for (int j = 0; j < freq; j++) {
-                nums[idx] = color;
-                idx++;
-            }
-        }        
+        int right = n-1;
+        while(idx<=right){
+        if(nums[idx]==0){
+            swap(nums[idx],nums[left]);
+            left++;
+            idx++;
+
+        }
+        else if(nums[idx]==2){
+            swap(nums[idx],nums[right]);
+            right--;
+        }
+        else 
+        {
+            idx++;
+        }  
+        }
     }
 };
